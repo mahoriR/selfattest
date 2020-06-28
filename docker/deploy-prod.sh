5a9477c2d64313721cd15ae13d337c2510b56aa2
rMahori <ravinder.nitks@gmail.com>
2020-06-28 15:48:13 +0530

    removed collectstatic from deployer Production API works

--- docker/deploy-prod.sh
+++ docker/deploy-prod.sh
@@ -5,6 +5,4 @@ git pull
 docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml up -d
 
-docker-compose -f docker-compose-prod.yml exec web_monolith mkdir static
-docker-compose -f docker-compose-prod.yml exec web_monolith python manage.py collectstatic
 docker-compose -f docker-compose-prod.yml exec web_monolith python manage.py migrate
