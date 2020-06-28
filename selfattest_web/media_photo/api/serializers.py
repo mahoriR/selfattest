ae4fa0010ab664b63d8551e158b769b414afbc89
rMahori <ravinder.nitks@gmail.com>
2020-06-28 15:55:16 +0530

    Removed photo_id from resposne, not relevant to user

--- selfattest_web/media_photo/api/serializers.py
+++ selfattest_web/media_photo/api/serializers.py
@@ -3,14 +3,9 @@ from ..models import UserPhotoMeta
 
 
 class UserPhotoSerializer(serializers.ModelSerializer):
-    photo_id = serializers.SerializerMethodField()
 
     class Meta:
         model = UserPhotoMeta
         fields = (
-            'photo_id',
             'photo_url',
         )
-
-    def get_photo_id(self, obj):
-        return str(obj.get_external_id())
